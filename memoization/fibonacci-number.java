2b6d83a2e2f81e564a6b9545101b774f561bf451
Indranil Mukhopadhyay <indranilm204@gmail.com>
2026-09-18 17:25:17 +0530

    LC-fibonacci-number [Runtime: N/A; Memory: N/A]

--- memoization/fibonacci-number.java
+++ memoization/fibonacci-number.java
@@ -1,12 +1,11 @@
 class Solution {
-    public int fibonacci(int a,int b, int n){
-        if(n==0){
-            return a;
-        }
-        return fibonacci(b,(a+b),n-1);
-    }
     public int fib(int n) {
-        int ans=fibonacci(0,1,n);
+        if(n==0||n==1){
+            return n;
+        }
+        int a1=fib(n-1);
+        int a2=fib(n-2);
+        int ans=a1+a2;
         return ans;
     }
 }
